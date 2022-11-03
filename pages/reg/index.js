fc26cf7d907fc4efc13fd7a3840e51148ee70a3b
Philix27 <philixbob@gmail.com>
2022-11-03 19:27:07 +0100

    added Link to a

--- pages/reg/index.js
+++ pages/reg/index.js
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import React from "react";
 import { Landing } from "../../comps/global/Landing";
 
@@ -10,24 +11,28 @@ export default function RegistrationPage() {
         subtitle="Follow to the steps to be come a member."
       />
       <div className="registration">
-        <a href="/reg/payment">
-          <div className="card">
-            <img src="/images/pay.jpg" alt="payment" />
-            <div className="inner">
-              <h3>Step 1</h3>
-              <p>Make a payment.</p>
+        <Link href="/reg/payment">
+          <a>
+            <div className="card">
+              <img src="/images/pay.jpg" alt="payment" />
+              <div className="inner">
+                <h3>Step 1</h3>
+                <p>Make a payment.</p>
+              </div>
             </div>
-          </div>
-        </a>
-        <a href="/reg/form">
-          <div className="card">
-            <img src="/images/form.jpg" alt="form" />
-            <div className="inner">
-              <h3>Step 2</h3>
-              <p>Fill the form.</p>
+          </a>
+        </Link>
+        <Link href="/reg/form">
+          <a>
+            <div className="card">
+              <img src="/images/form.jpg" alt="form" />
+              <div className="inner">
+                <h3>Step 2</h3>
+                <p>Fill the form.</p>
+              </div>
             </div>
-          </div>
-        </a>
+          </a>
+        </Link>
       </div>
     </div>
   );
